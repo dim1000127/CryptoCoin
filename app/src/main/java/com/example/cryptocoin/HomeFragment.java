715b18b4bb39e9f79af8c64ee4ce9535f0bfe953
dim1000127 <60386475+dim1000127@users.noreply.github.com>
2022-01-18 00:51:01 +0300

    Блоки на главном экране и работа с API
    На главном экране добавлены три блока для отображения то-3 криптовалют. Начата работа с API для получения данных.

--- app/src/main/java/com/example/cryptocoin/HomeFragment.java
+++ app/src/main/java/com/example/cryptocoin/HomeFragment.java
@@ -4,14 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
+    View rootView;
+    ImageView imageViewCryptoTop1;
+    ImageView imageViewCryptoTop2;
+    ImageView imageViewCryptoTop3;
+    TextView textViewSymbolCryptoOne;
+    TextView textViewSymbolCryptoTwo;
+    TextView textViewSymbolCryptoThree;
+    TextView textViewChange24hCryptoOne;
+    TextView textViewChange24hCryptoTwo;
+    TextView textViewChange24hCryptoThree;
+    TextView textViewPriceCryptoOne;
+    TextView textViewPriceCryptoTwo;
+    TextView textViewPriceCryptoThree;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
         return  inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
--- app/src/main/java/com/example/cryptocoin/Main.java
+++ app/src/main/java/com/example/cryptocoin/Main.java
@@ -1,24 +1,32 @@
 package com.example.cryptocoin;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Main extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
     Fragment selectedFragment;
+    ActionBar actionBar;
+    private static String apiKey = "908e2080-ad8d-4d43-bd0a-e65b8587d172";
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+        actionBar = getSupportActionBar();
 
         selectedFragment = new HomeFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
@@ -36,15 +44,15 @@ public class Main extends AppCompatActivity {
                     case R.id.navigation_home:
                         selectedFragment =  new HomeFragment();
                         //необходимо будет сделать свой ActionBar
-                        getSupportActionBar().setTitle("CryptoCoin");
+                        actionBar.setTitle("CryptoCoin");
                         break;
                     case R.id.navigation_list_top:
                         selectedFragment =  new ListFragment();
-                        getSupportActionBar().setTitle("Топ криптовалют");
+                        actionBar.setTitle("Топ криптовалют");
                         break;
                     case R.id.navigation_bool_learn:
                         selectedFragment =  new BookLearnFragment();
-                        getSupportActionBar().setTitle("Обучение");
+                        actionBar.setTitle("Обучение");
                         break;
                 }
                 if(selectedFragment != null){
@@ -53,4 +61,53 @@ public class Main extends AppCompatActivity {
                 return true;
             }
     };
+
+    public void APICall() {
+        String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+        /*List<NameValuePair> paratmers = new ArrayList<NameValuePair>();
+        paratmers.add(new BasicNameValuePair("start","1"));
+        paratmers.add(new BasicNameValuePair("limit","10"));
+        paratmers.add(new BasicNameValuePair("convert","USD"));*/
+
+        ContentValues paratmers = new ContentValues();
+        paratmers.put("start", "1");
+        paratmers.put("limit", "3");
+        paratmers.put("convert", "USD");
+
+        try {
+            String result = makeAPICall(uri, paratmers);
+            //System.out.println(result);
+        } catch (IOException e) {
+            System.out.println("Ошибка: нельзя получить доступ к содержимому - " + e.toString());
+        } catch (URISyntaxException e) {
+            System.out.println("Ошибка: неверный URL-адрес " + e.toString());
+        }
+    }
+
+    public static String makeAPICall(String uri, ContentValues parameters)
+            throws URISyntaxException, IOException {
+        String response_content = "";
+
+        /*URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }*/
+
+        return response_content;
+    }
 }
